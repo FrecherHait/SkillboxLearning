c32c6dc1f276137450b4b3de4f0509e3dfd8b228
Frecherhait <firefly2bravo@gmail.com>
2024-03-28 21:16:48 +0400

    Начал задание 14.8

--- Module_14/Objective_8/main.cpp
+++ Module_14/Objective_8/main.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
 
+struct Field{
+    bool isShipExist = false;
+    bool isShipDestroyed = false;
+    bool isMissShooting = false;
+};
+
+Field field_1[10][10] = {};
+Field field_2[10][10] = {};
+
+std::string checkPos(Field &m_fieldBay){
+    if (m_fieldBay.isShipExist)
+	return "S ";
+    else
+	return "• ";
+}
+
+void printFields(){
+    for(int x = 0; x < 10; x++){
+        std::string storeField_1 = "";
+        std::string storeField_2 = "";
+
+        for(int y = 0; y < 10; y++){
+	    storeField_1 += checkPos(field_1[x][y]);
+	    storeField_2 += checkPos(field_2[x][y]);
+        }
+	std::cout << storeField_1 + "  " + storeField_2 << std::endl;
+    }
+}
+
 int main() {
 #ifdef WIN32
     system("chcp 65001");
@@ -7,9 +36,7 @@ int main() {
     std::cout << "=========Модуль 14 \"Двумерные массивы и алгоритмы над ними\"=========" << std::endl;
     std::cout << "====================Задание 8 \"Морской бой\"====================" << std::endl;
 
-    std::string symbol = "•";
-
-    std::cout << symbol << std::endl;
+    printFields();
 
     return 0;
 }
