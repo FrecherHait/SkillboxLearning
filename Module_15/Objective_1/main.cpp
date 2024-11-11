bbb209e89bb169456e1f842829e763a27850c64d
Vichirev Denis <valeriyhait@frecherstudio.ru>
2024-11-11 13:19:46 +0400

    Доработал задачу 15.1

--- Module_15/Objective_1/main.cpp
+++ Module_15/Objective_1/main.cpp
@@ -1,26 +1,29 @@
-#include <locale>
 #include <iostream>
 
-int main(){
-    //std::locale ru("ru_RU.UTF8");
-    int numbers[9] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-
+std::pair<int, int> getResultIndexes(int numbers[], const int size){
     std::pair<int, int> indexes;
 
-    int result = 0;
+    int maxSum = 0;
 
-    for (int i = 0; i < 9; i++){
-	int temp = 0;
-	for (int j = 0; j < 9 - i; j++){
-	    temp += numbers[i+j];
-	    if (temp > result){
-		result = temp;
-		indexes = std::make_pair(i, i+j);
-	    }
-	}
+    for (int i = 0; i < size; i++){
+        for (int currentSum = 0, j = 0; j < size - i; j++){
+            currentSum += numbers[i+j];
+            if (currentSum > maxSum){
+                maxSum = currentSum;
+                indexes = std::make_pair(i, i+j);
+            }
+        }
     }
 
-    std::cout << "indexes(" << indexes.first << "," << indexes.second << ")" << std::endl; 
+    return indexes;
+}
+
+int main(){
+    int numbers[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int sizeNumberStorage = sizeof(numbers) / sizeof(*numbers);
+
+    auto [indexFirst, indexLast] = getResultIndexes(numbers, sizeNumberStorage);
+    std::cout << "Result indexes(" << indexFirst<< "," << indexLast << ")" << std::endl;
 
     return 0;
 }
