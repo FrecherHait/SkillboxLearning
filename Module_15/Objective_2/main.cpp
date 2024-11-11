1d324979c2c926a0e34ac1047fad5cc66f34f0f5
Vichirev Denis <valeriyhait@frecherstudio.ru>
2024-11-11 13:42:10 +0400

    Доработал задание 15.2

--- Module_15/Objective_2/main.cpp
+++ Module_15/Objective_2/main.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 
-int main(){
-    int numbers[4] = {2, 7, 11, 15};
-    int result = 26;
-
+std::pair<int, int> getValuesFromSum(int numbers[], int size, int result){
     std::pair<int, int> values;
 
     for (int i = 0; i < 4; i++){
-	for (int j = 1 + i; j < 4; j++){
-	    int sum = numbers[i] + numbers[j];
-	    if (sum == result){
-		values = std::make_pair(numbers[i], numbers[j]);
-		break;
-	    }
-	}
+        for (int j = 1 + i; j < 4; j++){
+            int sum = numbers[i] + numbers[j];
+            if (sum == result){
+                values = std::make_pair(numbers[i], numbers[j]);
+                break;
+            }
+        }
     }
 
-    std::cout << "Result: " << values.first << " " << values.second << std::endl;
+    return values;
+}
+
+int main(){
+    int numbers[4] = {2, 7, 11, 15};
+    int sizeNumberStorage = sizeof(numbers) / sizeof(*numbers);
+
+    auto [numFirst, numLast] = getValuesFromSum(numbers, sizeNumberStorage, 9);
+    std::cout << "Result: " << numFirst << " " << numLast << std::endl;
 
     return 0;
 }
