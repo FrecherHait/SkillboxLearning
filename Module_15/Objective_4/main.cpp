fc0e02105a68348fe08d9aa8297a9c2cbd0b213c
Vichirev Denis <valeriyhait@frecherstudio.ru>
2025-01-30 13:06:33 +0400

    Доработал задание 15.4

--- Module_15/Objective_4/main.cpp
+++ Module_15/Objective_4/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 #include <vector>
+
+void printElements(const std::string &msg, const std::vector<int> &elements){
+    std::cout << msg;
+
+    for (auto elem : elements){
+        std::cout << elem << " ";
+    }
+
+    std::cout << std::endl;
+}
+
+void sortElementsByAbsolute(std::vector<int> &numbers){
+    for (auto i = 0; i < numbers.size(); i++){
+        for (auto j = i + 1; j < numbers.size(); j++){
+            if (std::abs(numbers[j]) < std::abs(numbers[i])){
+                std::swap(numbers[i], numbers[j]);
+            }
+        }
+    }
+}
+
 int main(){
-    std::vector<int> numbers = { -7, -5, -3, -1, 2, 4, 6, 8 };
+    std::vector<int> numbers = { -100, -50, -5, 1, 10, 15 };
+
+    printElements("Elements until sort: ", numbers);
+    sortElementsByAbsolute(numbers);
+    printElements("Elements after sort: ", numbers);
+
     return 0;
 }
